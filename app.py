c42b36543d20410a106027ae9039a6dc4c05dd06
dormyyy <sania.crafter@gmail.com>
2023-03-02 16:58:58 +0200

    minor fix

--- app.py
+++ app.py
@@ -12,57 +12,63 @@ from utils.reformat import reformat
 
 load_dotenv()
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 
-# The ID and range of a sample spreadsheet.
-READ_SPREADSHEET_ID = '1cYhJToC6uCt78nh60bjQFaG9HCiG9q3lQAbpZH19G3E'
-DRIVE_SPREADSHEET_ID = '1X-gBG0-lZViNcmf_9knm0J36G5vNlvhjo0u_a79vHxE'
-SAMPLE_RANGE_NAME = 'Маковська'
+def main():
+    # If modifying these scopes, delete the file token.json.
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 
-"""Shows basic usage of the Sheets API.
-Prints values from a sample spreadsheet.
-"""
-creds = None
-# The file token.json stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.json', 'w') as token:
-        token.write(creds.to_json())
+    # The ID and range of a sample spreadsheet.
+    READ_SPREADSHEET_ID = '1cYhJToC6uCt78nh60bjQFaG9HCiG9q3lQAbpZH19G3E'
+    DRIVE_SPREADSHEET_ID = '1X-gBG0-lZViNcmf_9knm0J36G5vNlvhjo0u_a79vHxE'
+    SAMPLE_RANGE_NAME = 'Маковська'
 
-try:
-    service = build('sheets', 'v4', credentials=creds)
-    sheet = service.spreadsheets()
-    sheet_metadata = sheet.get(spreadsheetId=READ_SPREADSHEET_ID).execute()
-    properties = sheet_metadata.get('sheets')
-    sheets = [item.get('properties').get('title') for item in properties]
-    data = []
-    for i in sheets:
-        try:
-            data.append(read_table(sheet, READ_SPREADSHEET_ID, i))
-        except:
-            print(f"reading error occurred on {i}\n")
-    data = reformat(data)
-    sheet_metadata = sheet.get(spreadsheetId=DRIVE_SPREADSHEET_ID).execute()
-    properties = sheet_metadata.get('sheets')
-    sheets = [item.get('properties').get('title') for item in properties]
-    # drive_table(sheet, DRIVE_SPREADSHEET_ID, "PYTHON", data["PYTHON"])
-    for i in sheets:
-        try:
-            drive_table(sheet, DRIVE_SPREADSHEET_ID, i, data[i])
-        except:
-            print(f"driving error occurred on {i}\n")
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
-except HttpError as err:
-    print(err)
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+        sheet = service.spreadsheets()
+        sheet_metadata = sheet.get(spreadsheetId=READ_SPREADSHEET_ID).execute()
+        properties = sheet_metadata.get('sheets')
+        sheets = [item.get('properties').get('title') for item in properties]
+        data = []
+        for i in sheets:
+            try:
+                data.append(read_table(sheet, READ_SPREADSHEET_ID, i))
+            except:
+                print(f"reading error occurred on {i}\n")
+        data = reformat(data)
+        sheet_metadata = sheet.get(spreadsheetId=DRIVE_SPREADSHEET_ID).execute()
+        properties = sheet_metadata.get('sheets')
+        sheets = [item.get('properties').get('title') for item in properties]
+        # drive_table(sheet, DRIVE_SPREADSHEET_ID, "PYTHON", data["PYTHON"])
+        for i in sheets:
+            try:
+                drive_table(sheet, DRIVE_SPREADSHEET_ID, i, data[i])
+            except:
+                print(f"driving error occurred on {i}\n")
+
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == "__main__":
+    main()
